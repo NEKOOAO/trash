7d912faca21bffcfbc3dc078a8c408564564718d
BenChouUni <ben900202chou@gmail.com>
2022-12-03 18:07:33 +0800

    littel update

--- trash/Assets/script/sidewalk/Walker.cs
+++ trash/Assets/script/sidewalk/Walker.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class Walker : MonoBehaviour
 {
 
+    Vector2 throw_position = Vector2.zero;
     public void Walk(int speed)
     {
-        this.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        this.GetComponent<Rigidbody2D>().velocity = speed * Vector2.right;
     }
 
     public void Stop()
     {
-        this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
     }
 
     public void Throw(GameObject trash, Vector2 velocity,Trash trash_data)
     {
+        Stop();
         GameObject new_trash = Instantiate(trash, this.transform.position, Quaternion.identity);
         new_trash.GetComponent<TrashDisplay>().trash = trash_data;
         new_trash.GetComponent<TrashDisplay>().Show();
         new_trash.GetComponent<Rigidbody2D>().velocity = velocity;
+
 
     }
 }
